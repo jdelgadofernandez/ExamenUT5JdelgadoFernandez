00af4afbee6ed198c307e81272a27c73fc3de375
jdelgadofernandez <yoquesejoselola@gmail.com>
2023-05-02 10:37:45 +0200

    Commit de la documentacion

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"examen","l":"Bucles"},{"p":"examen","l":"Condicionales"},{"p":"examen","l":"MainExamen"},{"p":"examen","l":"TratamientoCadenas"}];updateSearchResults();
